fb4ca98beb6cd52fe34dfb8320068df778a2828a
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-03-19 08:23:55 -0600

    Se agregaron las data anotaciones en los modelos

--- EMOC20250319.AppWebMVC/Models/Brand.cs
+++ EMOC20250319.AppWebMVC/Models/Brand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EMOC20250319.AppWebMVC.Models;
 
@@ -7,8 +8,12 @@ public partial class Brand
 {
     public int BrandId { get; set; }
 
+    [Display(Name = "Marca")]
+    [Required(ErrorMessage = "La marca es obligatorio")]
     public string BrandName { get; set; } = null!;
 
+    [Display(Name = "Pais")]
+    [Required(ErrorMessage = "El pais es obligatorio")]
     public string? Country { get; set; }
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
--- EMOC20250319.AppWebMVC/Models/Category.cs
+++ EMOC20250319.AppWebMVC/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EMOC20250319.AppWebMVC.Models;
 
@@ -7,8 +8,10 @@ public partial class Category
 {
     public int CategoryId { get; set; }
 
+    [Display(Name = "Categoria")]
     public string CategoryName { get; set; } = null!;
 
+    [Display(Name = "Descripción")]
     public string? Description { get; set; }
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
